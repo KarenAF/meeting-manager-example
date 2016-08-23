6af067bb67651c8e46c2b9018a76667b7584cdd6
kareningenuity <karenjxliu@hotmail.com>
2016-08-22 20:18:55 -0500

    fixed index page to show json data. I didnt have scope meetings defined in the meetingsctrl.js file.

--- app/assets/javascripts/meetingsCtrl.js
+++ app/assets/javascripts/meetingsCtrl.js
@@ -5,9 +5,12 @@
 
   angular.module("app").controller("meetingsCtrl", function($scope, $http) {
     $http.get('/api/v1/meetings.json').then(function(response) {
+     $scope.meetings = response.data;
+
       console.log(response.data);
     });
   $scope.message = "Hello world";
+
 
     window.$scope = $scope;    
   });
