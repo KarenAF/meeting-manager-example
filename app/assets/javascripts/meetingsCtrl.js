102e7b9e0943331c1937dbc207fed353823dace5
kareningenuity <karenjxliu@hotmail.com>
2016-08-22 20:13:52 -0500

    added json to index.html.erb

--- app/assets/javascripts/meetingsCtrl.js
+++ app/assets/javascripts/meetingsCtrl.js
@@ -1,10 +1,13 @@
+/* global angular */
+
 (function () {
   "use strict";
 
-  angular.module("app").controller("meetingsCtrl", function($scope) {
-
-$scope.message = "Hello world";
-
+  angular.module("app").controller("meetingsCtrl", function($scope, $http) {
+    $http.get('/api/v1/meetings.json').then(function(response) {
+      console.log(response.data);
+    });
+  $scope.message = "Hello world";
 
     window.$scope = $scope;    
   });
