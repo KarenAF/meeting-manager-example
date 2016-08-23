db9d1055ec32ab433163da1adf7b6f9daf021761
kareningenuity <karenjxliu@hotmail.com>
2016-08-23 18:58:14 -0500

    added private restrict access code in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  before_action :restrict_access
+
+  private # this is a private method. This means other people can't use this. This doesn't really matter in Ruby.
+
+  def restrict_access
+    # check if the request has an API key as part of it...
 end
